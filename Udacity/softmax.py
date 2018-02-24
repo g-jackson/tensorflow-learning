9fcce2c4bb0e5fd97b0b7c4f2f24c033433ae1f9
Glenn Jackson <gjackson@live.ie>
2018-02-24 23:09:10 +0000

    Udacity repo added

---
+++ Udacity/softmax.py
@@ -0,0 +1,21 @@
+"""Softmax."""
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+
+
+def softmax(x):
+    """Compute softmax values for each sets of scores in x."""
+    e_x = np.exp(x - np.max(x))
+    return e_x / e_x.sum(axis=0)
+    #return np.array(softmax)
+
+scores = [1.0, 2.0, 3.0]
+print(softmax(scores))
+
+# Plot softmax curves
+x = np.arange(-2.0, 6.0, 0.1)
+scores = np.vstack([x, np.ones_like(x), 0.2 * np.ones_like(x)])
+
+plt.plot(x, softmax(scores).T, linewidth=2)
+plt.show()
